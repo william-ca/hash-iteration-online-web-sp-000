7fec5d87a9adf9a7d4e1c601b34cdc0d698efe43
Will Andre <wandre94@gmail.com>
2021-01-05 23:09:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -10,5 +10,3 @@ def happy_birthday(birthday_kids)
     end
   end
 end
-
-
